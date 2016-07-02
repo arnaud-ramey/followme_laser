41a81004e528538f8a0565f378d5d8a137796883
Arnaud Ramey <arnaud.a.ramey@gmail.com>
2016-07-02 15:53:17 +0200

    small modifs

--- include/followme_laser/timer.h
+++ include/followme_laser/timer.h
@@ -14,41 +14,29 @@
 #include <sys/time.h>
 #include <stdlib.h>
 #include <stdio.h>
-/*!
- * a timer class - it uses 'float' as a time type
- */
 class Timer {
 public:
-  typedef float Time;
-  const static Time NOTIME = -1;
-
-  Timer() {
-    reset();
-  }
-
-  ~Timer() {
-  }
-
-  //! reset time to 0
+  typedef double Time;
+  static const Time NOTIME = -1;
+  Timer() { reset(); }
   virtual inline void reset() {
     gettimeofday(&start, NULL);
-    //usleep(2000);
   }
-
   //! get the time since ctor or last reset (milliseconds)
-  virtual inline Time getTimeMilliseconds() const {
+  virtual inline Time getTimeSeconds() const {
     struct timeval end;
     gettimeofday(&end, NULL);
     return (Time) (// seconds
                    (end.tv_sec - start.tv_sec)
-                   * 1000 +
+                   +
                    // useconds
                    (end.tv_usec - start.tv_usec)
-                   / 1000.f);
+                   / 1E6);
   }
-
-  //! get the time since ctor or last reset (seconds)
-  virtual inline Time getTimeSeconds() const {
+private:
+  struct timeval start;
+}; // end class Timer
+al inline Time getTimeSeconds() const {
     return getTimeMilliseconds() / 1000.f;
   }
 
